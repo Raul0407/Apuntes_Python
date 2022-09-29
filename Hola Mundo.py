d1b5ca007a1648e565f831a7c5eb94d9e713ec0e
Raul <al_rlopezm@iessierradelasvillas.org>
2022-09-29 10:01:41 +0200

    Clase 29/09/2022

--- Hola Mundo.py
+++ Hola Mundo.py
@@ -1,1 +1,22 @@
-print("Hola Mundo")
+#Tipos de datos y variables
+
+#Numéricos
+#Enteros -- int
+#Real -- float
+#Cadenas de carácteres -- str
+#Lógico -- bool
+
+nombre = "Raúl"
+edad = 17
+mayor_O_menor_edad = False  
+print(type(mayor_O_menor_edad))
+
+# Entrada y salida de datos
+#Salida con print()
+print("Buenos días", (nombre), "Tu edad es", (edad))
+
+#Entrada de datos -- input()
+
+nombre = input("Dime tu nombre:\n ")
+edad = input("Dime tu edad:\n ")
+print("Buenos días", (nombre), "tu edad es", (edad))
