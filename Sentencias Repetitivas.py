553d3729bf3cd616f98905a11ddebc0fa8619693
Raul <al_rlopezm@iessierradelasvillas.org>
2022-10-24 11:16:29 +0200

    Clase 24/10/2022 Sentencias Repetitivas

---
+++ Sentencias Repetitivas.py
@@ -0,0 +1,17 @@
+#SENTENCIAS REPETITIVAS
+
+#while
+
+"""
+contador = 1
+while contador<=10:
+    print(contador)
+    contador += 1
+"""
+
+#for
+#lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+vNombres = ["Ángel", "María", "Pepe", "Jose"]
+
+for numeros in vNombres:
+    print(numeros)
